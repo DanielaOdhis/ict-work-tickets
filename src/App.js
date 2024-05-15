12010f8ffefe0d24c9f498c88896cda2d1dd8a97
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-05-15 11:48:39 +0300

    Creating automated work tickets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className="top-section">
+        <div className="container">
+          <h1>State Department For Energy</h1>
+          <address>
+            Kawi Complex, Off Red Cross Rd, Nairobi, <br />
+            Nairobi. <br />
+            Phone: +254(0) 20 4841000 <br />
+            <a href="mailto:info@energy.go.ke">info@energy.go.ke</a> <br />
+            <a href="https://energy.go.ke">https://energy.go.ke</a>
+          </address>
+        </div>
+        <div className="order">
+          <h2>ICT Work Ticket</h2>
+          <table>
+            <tr>
+              <td className="work"><b>WORK ORDER NAME(S)</b></td>
+            </tr>
+            <tr>
+              <td className="name"><b>Alex Morara</b></td>
+            </tr>
+          </table>
+          <br />
+          <table>
+            <tr>
+              <td className="work"><b>DATE</b></td>
+            </tr>
+            <tr>
+              <td className="name"><b>09-May-2024 10:14</b></td>
+            </tr>
+          </table>
+          <br />
+          <b>Ticket No: MOEICT65406854</b>
+        </div>
+      </div>
+      <div className="request">
+        <table>
+          <tr>
+            <td className="names"><b>REQUESTED BY</b></td>
+            <td className="names"><b>Telephone/Official Email</b></td>
+            <td className="names"><b>FLOOR/ DEPARTMENT</b></td>
+          </tr>
+          <tr>
+            <td className="num"><b>Peninah Munyao</b></td>
+            <td className="num"><b>20 484100</b></td>
+            <td className="num"><b>M1, Accounts-Imprest</b></td>
+          </tr>
+        </table>
+      </div>
+      <div className='reports'>
+        <table>
+          <tr>
+            <td className="names"><b>REPORTED ISSUE</b></td>
+            <td className='names'><b>JOB DETAILS</b></td>
+          </tr>
+          <tr>
+            <td>Printer Configuration</td>
+            <td>0</td>
+          </tr>
+        </table>
+      </div>
+      <hr/>
+      <div></div>
     </div>
   );
 }
